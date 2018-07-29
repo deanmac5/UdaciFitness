666eba9ac5044325130574c0b29c8d796a97ef45
Dean Macaulay <dean@magpies.net>
2018-07-30 07:34:58 +1000

    Added tab navigation

--- App.js
+++ App.js
@@ -1,9 +1,45 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableHighlight, TouchableNativeFeedback, TouchableOpactity, TouchableWithoutFeedback } from 'react-native';
+import { View, Text, Platform, StyleSheet, TouchableHighlight, TouchableNativeFeedback, TouchableOpactity, TouchableWithoutFeedback } from 'react-native';
 import AddEntry from './components/AddEntry';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
+import History from './components/history'
+import { TabNavigator } from 'react-navigation'
+import { purple, white } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons' 
+
+const Tabs = TabNavigator({
+  History: {
+    screen: History,
+    navigationOptions: {
+      tabBarLabel: 'History',
+      tabBarIcon: ({ tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    }
+  },
+  AddEntry: {
+    screen: AddEntry,
+    navigationOptions: {
+      tabBarLabel: 'Add Entry',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    }
+  }
+}, {
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0,0,0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
 
 
 export default class App extends React.Component {
@@ -11,7 +47,9 @@ export default class App extends React.Component {
     return (
       <Provider store={createStore(reducer)}>
       <View style={{flex: 1}}>
-        <AddEntry />
+      <View style={{height: 20}}/>
+       
+        <Tabs />
       </View>
       </Provider>
     );
